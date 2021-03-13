ccba16b4f686ed30ccf92982917e9753475800ee
James <james.jin@efrei.net>
2021-03-13 19:04:53 +0100

    feat: add Quiz 2 and initInfo

---
+++ app/src/main/java/com/example/animequizz/QuizActivity2.java
@@ -0,0 +1,41 @@
+package com.example.animequizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuizActivity2 extends AppCompatActivity {
+
+    private String username;
+    private int difficulty;
+    private List<Anime> animeList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz2);
+        initInfo(savedInstanceState);
+    }
+    public void initInfo(Bundle savedInstanceState){
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                username= null;
+                difficulty = 0;
+                animeList = null;
+            } else {
+                username= extras.getString("username");
+                difficulty = extras.getInt("difficulty");
+                animeList = (List<Anime>) extras.getSerializable("animeList");
+            }
+        } else {
+            username = (String) savedInstanceState.getSerializable("username");
+            difficulty = (int) savedInstanceState.getSerializable("difficulty");
+            animeList = (List<Anime>) savedInstanceState.getSerializable("animeList");
+        }
+    }
+
+}
