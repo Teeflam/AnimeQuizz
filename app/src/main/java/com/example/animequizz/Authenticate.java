dad4890703c562177346b8102fba3bb7c555c017
Teeflam <franck.di@efrei.net>
2021-03-13 15:11:33 +0100

    Authenticate

---
+++ app/src/main/java/com/example/animequizz/Authenticate.java
@@ -0,0 +1,34 @@
+package com.example.animequizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Authenticate extends AppCompatActivity {
+    private TextView username;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authenticate);
+
+        Button button = (Button) findViewById(R.id.getUsername);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //go to another activity (list activity)
+                username = (TextView) findViewById(R.id.username);
+                Intent myIntent = new Intent(Authenticate.this, MainActivity.class);
+                myIntent.putExtra( "username",username.getText().toString());
+                Authenticate.this.startActivity(myIntent);
+            }
+        });
+
+
+        }
+}
+
--- app/src/main/java/com/example/animequizz/MainActivity.java
+++ app/src/main/java/com/example/animequizz/MainActivity.java
@@ -3,13 +3,30 @@ package com.example.animequizz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String username;
+    private TextView header;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        header = (TextView) findViewById(R.id.header);
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                username= null;
+            } else {
+                username= extras.getString("username");
+            }
+        } else {
+            username= (String) savedInstanceState.getSerializable("username");
+        }
+        header.setText("Welcome " +username + ", to the Anime Quizz Game" );
+
+
 
 
     }
