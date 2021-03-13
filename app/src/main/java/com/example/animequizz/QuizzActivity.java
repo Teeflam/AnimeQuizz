19774d30f78ca93898e80672c52847d3ca2daaa9
James <james.jin@efrei.net>
2021-03-13 15:49:46 +0100

    quiz activity get extra information and fill the layout

--- app/src/main/java/com/example/animequizz/QuizzActivity.java
+++ app/src/main/java/com/example/animequizz/QuizzActivity.java
@@ -10,5 +10,21 @@ public class QuizzActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizz);
+        String username;
+        int difficulty;
+        //get the extra information sended through intent
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                username= null;
+                difficulty = 0;
+            } else {
+                username= extras.getString("username");
+                difficulty = extras.getInt("difficulty");
+            }
+        } else {
+            username= (String) savedInstanceState.getSerializable("username");
+        }
+
     }
 }
